dc6db345c85099244cfeec39b4718c0e65d038c5
Burton Jaursch <jaurschb@oregonstate.edu>
2017-10-10 19:10:17 -0700

    Deleting alg.cpp & adding example code #1

--- select.cpp
+++ select.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
 int main(){
-	string ;
+	ifstream InputFile;
+	InputFile.open("input.txt"); 
 	
+	int m, n, k;
+       	string input;
 
+	getline(InputFile, input, ',');
+	m = stoi(input); //amount of binary files
+
+	getline(InputFile, input, ',');
+	n = stoi(input); //amount of numbers in b. files
+
+	getline(InputFile, input);
+	k = stoi(input); //lowest number
+
+	InputFile.close();	
+	
+	ifstream BinaryFile;
+
+	
 	return 0;
 } 
